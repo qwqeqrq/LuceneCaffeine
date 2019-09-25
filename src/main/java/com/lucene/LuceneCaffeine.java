236c814d2eebe6ccfd125c4bf8106558c33e2b12
zhangww1 <Zw18829288843>
2019-09-25 10:10:31 +0800

    实现CommandLineRunner  在springboot启动时加载方法执行 wait(100)到时自动唤醒

--- src/main/java/com/lucene/LuceneCaffeine.java
+++ src/main/java/com/lucene/LuceneCaffeine.java
@@ -1,16 +1,30 @@
 package com.lucene;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LuceneCaffeine {
+public class LuceneCaffeine implements CommandLineRunner {
 
     /**
      * 项目入口启动类
-     *
      */
     public static void main(String[] args) {
-        SpringApplication.run(LuceneCaffeine.class,args);
+        SpringApplication.run(LuceneCaffeine.class, args);
+        System.out.println("项目启动成功");
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (; ; ) {
+            synchronized (this) {
+                wait(100);
+                System.out.println("能自动释放锁嘛");
+                System.out.println("===========");
+            }
+        }
     }
 }
